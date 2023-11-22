fb2a2409c05fddd05bef3f81a571767a89347992
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-11-22 01:10:36 -0500

    Optimize structure and add comments

--- src/llm_answer.py
+++ src/llm_answer.py
@@ -1,99 +1,91 @@
 import time
 import os
-from fetch_web_content import fetch_web_content
-from retrieval import embeddding_retrieval
+import yaml
+from fetch_web_content import WebContentFetcher
+from retrieval import EmbeddingRetriever
 from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.schema import HumanMessage
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.schema import (
-    HumanMessage,
-)
 
-TOP_K = 10
+class GPTAnswer:
+    TOP_K = 10  # Top K documents to retrieve
 
-def getAnswer(model_name: str, api_key: str, query: str, relevant_docs: str, language: str, output_format: str, profile: str):
-    
-    llm = ChatOpenAI(model_name=model_name, openai_api_key=api_key, temperature=0.0, streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
+    def __init__(self):
+        # Load configuration from a YAML file
+        config_path = os.path.join(os.path.dirname(__file__), 'config', 'config.yaml')
+        with open(config_path, 'r') as file:
+            self.config = yaml.safe_load(file)
+        self.model_name = self.config["model_name"]
+        self.api_key = self.config["openai_api_key"]
 
-    template = \
-        r"""
-        Web search result:
-        {context_str}
+    def _format_reference(self, relevant_docs_list, link_list):
+        # Format the references from the retrieved documents for use in the prompt
+        reference_url_list = [(relevant_docs_list[i].metadata)['url'] for i in range(self.TOP_K)]
+        reference_content_list = [relevant_docs_list[i].page_content for i in range(self.TOP_K)]
+        reference_index_list = [link_list.index(link)+1 for link in reference_url_list]
+        rearranged_index_list = self._rearrange_index(reference_index_list)
+
+        # Create a formatted string of references
+        formatted_reference = "\n"
+        for i in range(self.TOP_K):
+            formatted_reference += ('Webpage[' + str(rearranged_index_list[i]) + '], url: ' + reference_url_list[i] + ':\n' + reference_content_list[i] + '\n\n\n')
+        return formatted_reference
+
+    def _rearrange_index(self, original_index_list):
+        # Rearrange indices to ensure they are unique and sequential
+        index_dict = {}
+        rearranged_index_list = []
+        for index in original_index_list:
+            if index not in index_dict:
+                index_dict.update({index: len(index_dict)+1})
+                rearranged_index_list.append(len(index_dict))
+            else:
+                rearranged_index_list.append(index_dict[index])
+        return rearranged_index_list
+
+    def get_answer(self, query, relevant_docs, language, output_format, profile):
+        # Create an instance of ChatOpenAI and generate an answer
+        llm = ChatOpenAI(model_name=self.model_name, openai_api_key=self.api_key, temperature=0.0, streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
         
-        Instructions: You are a/an {profile}. Using the provided web search results, write a comprehensive and detailed reply to the given query. 
-        Make sure to cite results using [number] notation after the reference.
-        At the end of the answer, list the corresponding references with indexes, each reference contains the urls and quoted sentences from the web search results by the order you marked in the answer above and these sentences should be exactly the same as in the web search results.
-        Here is an example of a reference:
-            [1] URL: [https://www.pocketgamer.biz/news/81670/tencent-and-netease-dominated-among-chinas-top-developers-in-q1/]
-                Quoted sentence: Tencent accounted for roughly 50% of domestic market revenue for the quarter, compared to 40% in Q1 2022.
-        
-        Answer in language: {language}
-        Query: {query}
-        Output Format: {format}
-        Please organize your output according to the Output Format. If the Output Format is empty, you can ignore it.
-        """
+        template = self.config["template"]
+        prompt_template = PromptTemplate(
+            input_variables=["profile", "context_str", "language", "query", "format"],
+            template=template
+        )
 
-    prompt_template = PromptTemplate(
-        input_variables=["profile", "context_str", "language", "query", "format"],
-        template=template
-    )
+        profile = "conscientious researcher" if not profile else profile
+        summary_prompt = prompt_template.format(context_str=relevant_docs, language=language, query=query, format=output_format, profile=profile)
+        print("\n\nThe message sent to LLM:\n", summary_prompt)
+        print("\n\n", "="*30, "GPT's Answer: ", "="*30, "\n")
+        gpt_answer = llm([HumanMessage(content=summary_prompt)])
 
-    profile = "conscientious researcher" if profile == "" else profile
-    summary_prompt = prompt_template.format(context_str = relevant_docs, language=language, query=query, format=output_format, profile=profile)
-    print("\n\nThe message you sent to LLM:\n", summary_prompt)
-    print("\n\n", "="*30, "GPT's Answer: ", "="*30, "\n")
-    gpt_answer = llm([HumanMessage(content=summary_prompt)])
+        return gpt_answer
 
-    return gpt_answer
-
-def format_reference(relevant_docs_list: list, link_list: list):
-    
-    reference_url_list = [(relevant_docs_list[i].metadata)['url'] for i in range(TOP_K)]
-    reference_content_list = [relevant_docs_list[i].page_content for i in range(TOP_K)]
-    reference_index_list = [link_list.index(link)+1 for link in reference_url_list]
-    rearranged_index_list = rearrange_index(reference_index_list)
-    
-    # sorted_contents = [content for _, content in sorted(zip(reference_index_list, reference_content_list))]
-    # sorted_index_list = [sorted(set(reference_index_list)).index(i) + 1 for i in reference_index_list]
-
-    formatted_reference = "\n"
-
-    for i in range(TOP_K):
-        formatted_reference += ('Webpage[' + str(rearranged_index_list[i]) + '], url: ' + reference_url_list[i] + ':\n' + reference_content_list[i] + '\n\n\n')
-
-    return formatted_reference
-
-def rearrange_index(original_index_list: list):
-    index_dict = {}
-    rearranged_index_list = []
-
-    for index in original_index_list:
-        if index not in index_dict: 
-            index_dict.update({index: len(index_dict)+1})
-            rearranged_index_list.append(len(index_dict))
-        else:
-            rearranged_index_list.append(index_dict[index])
-
-    return rearranged_index_list
-
-# Testing this code:
+# Example usage
 if __name__ == "__main__":
-
-    query = "Tencent Game's Global Market Share"
-    model_name = "gpt-3.5-turbo-16k"
-    api_key = ""
+    content_processor = GPTAnswer()
+    query = "What happened to Silicon Valley Bank"
     output_format = ""
     profile = ""
-    
-    web_contents, serper_response = fetch_web_content(query)
-    relevant_docs_list = embeddding_retrieval(web_contents, serper_response['links'], query)
-    formatted_relevant_docs = format_reference(relevant_docs_list, serper_response['links'])
+
+    # Fetch web content based on the query
+    web_contents_fetcher = WebContentFetcher(query)
+    web_contents, serper_response = web_contents_fetcher.fetch()
+
+    # Retrieve relevant documents using embeddings
+    retriever = EmbeddingRetriever()
+    relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
+    formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
+
+    print(relevant_docs_list)
 
     print(formatted_relevant_docs)
 
+    # Measure the time taken to get an answer from the GPT model
     start = time.time()
-    ai_message_obj = getAnswer(model_name, api_key, query, formatted_relevant_docs, serper_response['language'], output_format, profile)
-    answer = ai_message_obj.content + '\n' # Necessary for subsequent matching!!
+    ai_message_obj = content_processor.get_answer(query, formatted_relevant_docs, serper_response['language'], output_format, profile)
+    answer = ai_message_obj.content + '\n'
     end = time.time()
 
-    print("\nGPT Answer time:", end - start, "s")
+    print("\n\nGPT Answer time:", end - start, "s")
