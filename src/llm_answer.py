9c652d670b2a008595afb5eb1db9253173dacf1c
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-11-22 15:21:20 -0500

    Optimize structure and add comments

--- src/llm_answer.py
+++ src/llm_answer.py
@@ -66,8 +66,8 @@ class GPTAnswer:
 if __name__ == "__main__":
     content_processor = GPTAnswer()
     query = "What happened to Silicon Valley Bank"
-    output_format = ""
-    profile = ""
+    output_format = "" # User can specify output format
+    profile = "" # User can define the role for LLM
 
     # Fetch web content based on the query
     web_contents_fetcher = WebContentFetcher(query)
@@ -77,15 +77,13 @@ if __name__ == "__main__":
     retriever = EmbeddingRetriever()
     relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
     formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
-
-    print(relevant_docs_list)
-
     print(formatted_relevant_docs)
 
     # Measure the time taken to get an answer from the GPT model
     start = time.time()
+
+    # Generate answer from ChatOpenAI
     ai_message_obj = content_processor.get_answer(query, formatted_relevant_docs, serper_response['language'], output_format, profile)
     answer = ai_message_obj.content + '\n'
     end = time.time()
-
     print("\n\nGPT Answer time:", end - start, "s")
--- src/main.py
+++ src/main.py
@@ -1,31 +1,29 @@
+from fetch_web_content import WebContentFetcher
+from retrieval import EmbeddingRetriever
+from llm_answer import GPTAnswer
 import time
-import json
-from fetch_web_content import fetch_web_content
-from llm_answer import getAnswer, format_reference
-from retrieval import embeddding_retrieval
-from locate_reference import locate_source
 
 if __name__ == "__main__":
-    query = "Tencent Game's Global Market Share"
-    model_name = "gpt-3.5-turbo-16k"
-    api_key = ""
-    output_format = ""
-    profile = ""
-    
-    web_contents, serper_response = fetch_web_content(query)
-    relevant_docs_list = embeddding_retrieval(web_contents, serper_response['links'], query)
-    formatted_relevant_docs = format_reference(relevant_docs_list, serper_response['links'])
+    query = "What happened to Silicon Valley Bank"
+    output_format = "" # User can specify output format
+    profile = "" # User can define the role for LLM
 
+    # Fetch web content based on the query
+    web_contents_fetcher = WebContentFetcher(query)
+    web_contents, serper_response = web_contents_fetcher.fetch()
+
+    # Retrieve relevant documents using embeddings
+    retriever = EmbeddingRetriever()
+    relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
+    content_processor = GPTAnswer()
+    formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
     print(formatted_relevant_docs)
 
+    # Measure the time taken to get an answer from the GPT model
     start = time.time()
-    ai_message_obj = getAnswer(model_name, api_key, query, formatted_relevant_docs, serper_response['language'], output_format, profile)
-    answer = ai_message_obj.content + '\n' # Necessary for subsequent matching!!
+
+    # Generate answer from ChatOpenAI
+    ai_message_obj = content_processor.get_answer(query, formatted_relevant_docs, serper_response['language'], output_format, profile)
+    answer = ai_message_obj.content + '\n'
     end = time.time()
-
-    reference_cards = locate_source(answer, serper_response)
-    json_formatted_cards = json.dumps(reference_cards, indent=4)
-
-
-    print('\n\n', '='*30, "Reference cards", "="*30, "\n")
-    print(json_formatted_cards)
+    print("\n\nGPT Answer time:", end - start, "s")
